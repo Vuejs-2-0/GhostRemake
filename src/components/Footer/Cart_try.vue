bd1655472d12b000da89d8ad6fdb46b570490ef1
Jialegoh <cteh60112@gmail.com>
2024-06-18 23:06:16 +0800

    cart footer work

--- src/components/Footer/Cart_try.vue
+++ src/components/Footer/Cart_try.vue
@@ -9,11 +9,12 @@
       </DialogHeader>
       <div class="grid gap-4 py-4 overflow-y-auto px-6">
         <div class="flex flex-col justify-between">
-          <div v-for="product in product" :key="product.id" class="product-card flex justify-between items-center p-4 bg-white">
-            <!-- <img :src="product.media.image" alt="Product Image" class="w-16 h-16 rounded object-cover" /> -->
+          <div v-for="product in productList" :key="product.id" class="product-card flex justify-between items-center p-4 bg-white">
+            <img :src="product.media.image" alt="Product Image" class="w-16 h-16 rounded object-cover" />
             <div class="product-info flex-grow ml-4">
               <h3 class="text-lg font-semibold">{{ product.name }}</h3>
               <p class="text-sm text-gray-500">RM{{ product.price }}</p>
+              <p class="text-sm text-gray-500">Quantity: {{ product.quantity }}</p>
             </div>
             <div class="actions flex justify-end items-center">
               <AddButton client:only="vue" :product="product" />
@@ -47,29 +48,20 @@ import { useStore } from '@nanostores/vue';
 import AddButton from '@/components/Product/AddButton.vue';
 
 const $cart = useStore(cart);
-const prop = defineProps(['product']);
-const { products} = toRefs(prop);
+const props = defineProps(['product']);
+const { product } = toRefs(props);
 
 const productList = computed(() => {
   if (!$cart.value.items) return [];
-  return products.value.filter(product => {
-    return $cart.value.items.hasOwnProperty(product.id);
-  }).map(product => {
-    return {
-      ...product,
-      quantity: $cart.value.items[product.id]
-    };
-  });
+  return product.value.filter(p => $cart.value.items[p.id])
+                      .map(p => ({ ...p, quantity: $cart.value.items[p.id] }));
 });
 
 const totalItems = computed(() => {
   if (!$cart.value.items) return 0;
-
   let items = Object.entries($cart.value.items);
-
   return items.reduce((acc, [item_id, quantity]) => {
     return acc + quantity;
   }, 0);
 });
-
 </script>
