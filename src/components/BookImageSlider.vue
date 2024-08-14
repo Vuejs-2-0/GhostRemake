39b0362a8292369a0ad743e9b7a33ad5346e0fd8
JunyaoC <junyaoxiandingchan@gmail.com>
2024-08-14 21:23:35 +0800

    feat: Update book image and slider component

---
+++ src/components/BookImageSlider.vue
@@ -0,0 +1,64 @@
+<template>
+
+    <div class="w-full">
+        <div class="swiper">
+            <div class="swiper-wrapper">
+            <div v-for="image in images" class="swiper-slide  aspect-square overflow-hidden rounded-xl ">
+                <img class="w-full h-full object-cover" :src="image" alt="">
+            </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+import { onMounted, toRefs, ref } from 'vue';
+
+// const props = defineProps({
+//     images: Array,
+//     options: Object
+// })
+
+// const { images, options } = toRefs(props)
+
+const images = ref([
+    '/book7.jpg',
+    '/img/new_book.jpg',
+    '/img/book_7_end.jpg'
+])
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+onMounted(() => {
+    const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+        delay: 1000,
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+      },
+
+    });
+})
+
+// // init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+  
+// });
+
+</script>
+
+
+<style>
+.swiper-pagination-bullet-active {
+    background-color: #ffffff!important;
+}
+</style>
