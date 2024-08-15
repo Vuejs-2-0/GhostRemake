d86cbdebb1202dbd6b069a88d52e26447a337f0d
Jialegoh <cteh60112@gmail.com>
2024-08-15 09:38:48 +0800

    Ask them if really wanna leave and pay buton in acc history page

--- src/components/PurchaseHistory.vue
+++ src/components/PurchaseHistory.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { toRefs } from 'vue';
 import Summary from "../components/CheckoutDialog/Summary.vue";
+import { Button } from "@/components/ui/button";
 
 const props = defineProps(["tx"]);
 const { tx:txs } = toRefs(props);
@@ -39,7 +40,6 @@ let status_map = {
     }
 
     const paymentPage = (tx_uuid) => {
-        console.log(tx_uuid);
         if (typeof window !== 'undefined') {
             // This code will only run in the browser
             window.location.href = `/pay?tx=${tx_uuid}`;
@@ -52,15 +52,22 @@ let status_map = {
     <div class="w-full flex justify-center items-center ">
         <div class="w-full space-y-4">
             <article v-for="tx in txs" class="w-full flex justify-center items-center flex-col border-b-2 border-b-salmon-100 py-8">
-                <div class="w-full max-w-sm p-4">
-                    <div class=" flex justify-start items-center space-x-2">
-                        <div class="text-2xl mt-2">{{ computedStatus(tx)?.emoji }}</div>
-                        <div class="font-bold text-2xl">{{ computedStatus(tx)?.title }}</div>
+                <div class="flex justify-between items-center w-full max-w-sm p-4">
+                    <div class="w-full max-w-sm p-4">
+                        <div class=" flex justify-start items-center space-x-2">
+                            <div class="text-2xl mt-2">{{ computedStatus(tx)?.emoji }}</div>
+                            <div class="font-bold text-2xl">{{ computedStatus(tx)?.title }}</div>
+                        </div>
+                        <p>{{ computedStatus(tx)?.description }}</p>
                     </div>
-                    <p>{{ computedStatus(tx)?.description }}</p>
+                    <div>
+                        <Button v-if="tx.status === 'pending'" @click="paymentPage(tx.uuid)" class="inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 h-10 px-4 py-2 bg-white hover:bg-salmon-500 hover:text-white text-salmon-500 text-lg duration-300">
+                            前往付款
+                        </Button>
+                    </div>
                 </div>
                 <div class="w-full max-w-sm bg-white p-4 rounded-xl ">
-                    <button @click="paymentPage(tx.uuid)">Direct to payment page</button>
+                    
                     <Summary :postageEntry="tx.entries.filter((entry) => entry.type === 'postage')" :productEntries="tx.entries.filter((entry) => entry.type == 'product')" :form="tx.form" :value="tx.value"></Summary>
                 </div>
             </article> 
---
+++ src/layouts/HomeButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <Button @click="handleGoHomeClick" class="inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 h-10 px-4 py-2 bg-white hover:bg-salmon-500 hover:text-white text-salmon-500 text-lg duration-300">
+      返回主页
+    </Button>
+  </template>
+  
+  <script setup>
+  import { onBeforeUnmount} from 'vue';
+  import { Button } from "@/components/ui/button";
+  
+  const handleGoHomeClick = () => {
+    window.onbeforeunload = () => true;
+    window.location.href = "/";
+  };
+  
+  onBeforeUnmount(() => {
+    window.onbeforeunload = null;
+  });
+  </script>
